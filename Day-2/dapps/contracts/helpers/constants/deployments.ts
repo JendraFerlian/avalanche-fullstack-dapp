e59e51229bbde27866fd29d9f0cb261546ce57a9
JendraFerlian <ferlianjendra7@gmail.com>
2026-01-15 16:22:42 +0700

    Delete Day-2 directory

--- Day-2/dapps/contracts/helpers/constants/deployments.ts
+++
@@ -1,5 +0,0 @@
-import dotenv from "dotenv";
-
-dotenv.config();
-
-export const USER_PRIVATE_KEY = process.env.PRIVATE_KEY || "game gloom adapt oval leaf dove isolate soup lend faith carpet assault ticket alien student purity spawn achieve false habit kit crop urban region";
